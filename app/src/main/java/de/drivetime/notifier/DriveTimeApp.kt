bb5906866856b20c0fec4dc65265a582e66c2502
3115a083 <9198182+3115a083@users.noreply.github.com>
2026-09-01 09:25:47 +0200

    Add selectable routing providers and strict automation scheduling

--- app/src/main/java/de/drivetime/notifier/DriveTimeApp.kt
+++ app/src/main/java/de/drivetime/notifier/DriveTimeApp.kt
@@ -1,11 +1,5 @@
 package de.drivetime.notifier
 
 import android.app.Application
-import de.drivetime.notifier.automation.AutomationScheduler
 
-class DriveTimeApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        AutomationScheduler.schedule(this)
-    }
-}
+class DriveTimeApp : Application()
